98bfbf98918506cd8a74a446ca60351260b9d990
Ryan <vanhalenisagod@gmail.com>
2014-03-25 00:24:46 -0700

    Flashlight and Sound Edits
    Finished the flashlight feature. Flashlight now toggles with sound,
    drains, and charges up slowly by pressing "c".
    
    Changed flashlight color to a light cyan for cooler feel. Modified
    spooky sounds to be quieter and audible only at source.

--- Assets/Scripts/FlashLight Test.js
+++ Assets/Scripts/FlashLight Test.js
@@ -1,8 +1,18 @@
 var lightSource : Light; //Connect the light source in the Inspector
 static var energy : float = 100; //The energy amount of the flashlight
 static var turnedOn : boolean = false; //Boolean to check whether it's turned on or off
+static var charged : boolean = true;
 var drainSpeed : float = 2.0; //The speed that the energy is drained
+var chargeSpeed : float = 2.0; //The speed at which the energy is charged
+var toggleSound : AudioSource;
+
+//Battery Textures
 var batteryGUI : GUITexture;
+var batteryGUI100 :Texture2D;
+var batteryGUI75 : Texture2D;
+var batteryGUI50 : Texture2D;
+var batteryGUI25 : Texture2D;
+var batteryGUIEmpty : Texture2D;
 
 function Start()
 {
@@ -12,54 +22,86 @@ function Update ()
 {
     if (Input.GetButtonUp("Flashlight")) 
     	ToggleFlashlight();
-    if (Input.GetButtonUp("Charge"))
-    	RechargeEnergy();
 }
 
 //When the player press F we toggle the flashlight on and off
 function ToggleFlashlight() 
 {
     turnedOn = !turnedOn;
-    if (turnedOn && energy>0) 
+	toggleSound.Play();
+	
+    if (turnedOn) 
     {
-       TurnOnAndDrainEnergy();
-       
-    }
-     else
-     	lightSource.enabled = false;
-     	batteryGUI.enabled = false;
+	    lightSource.enabled = true;
+		batteryGUI.enabled = true;
+		DrainEnergy();
+	}
+    else
+    {
+		lightSource.enabled = false;
+		batteryGUI.enabled = false;
+	}
 }
 
 //When the flashlight is turned on we enter a while loop which drains the energy
-function TurnOnAndDrainEnergy () 
-{
-    lightSource.enabled = true;
+function DrainEnergy () 
+{		
+    while (turnedOn) 
+    {		
+		if (Input.GetButtonUp("Charge"))
+		{
+			charged = false;
+			RechargeEnergy();
+		}
+		
+		if(charged)
+		{
+			energy -= drainSpeed*Time.deltaTime;
+			lightSource.intensity = energy / 80;
+
+			if(energy > 75)
+				batteryGUI.texture = batteryGUI100;
+			if(energy <= 75)
+				batteryGUI.texture = batteryGUI75;
+			if(energy <= 50)
+				batteryGUI.texture = batteryGUI50;
+			if(energy <= 25)
+				batteryGUI.texture = batteryGUI25;
+			if(energy <= 0)
+			{
+				charged = false;
+				batteryGUI.texture = batteryGUIEmpty;
+			}
+		}
+	
+		yield;
+		Debug.Log(energy);
+	}   
     
-    
-    while (turnedOn && energy>0) 
-    {
-       batteryGUI.enabled = true;
-       energy -= drainSpeed*Time.deltaTime;
-       if(energy <= 16)
-       	lightSource.intensity = energy / 40;
-       		
-       yield;
-    }
-    turnedOn = false;
-    lightSource.enabled = false;
-    batteryGUI.enabled = false;
 }
 
 function RechargeEnergy()
 {
-	turnedOn = true;
-	while(turnedOn && energy < 100)
+	while(!charged)
 	{
-		energy += drainSpeed*Time.deltaTime;
-		lightSource.intensity = energy / 40;
+		energy += chargeSpeed*Time.deltaTime;
+		lightSource.intensity = energy / 80;
+		
+		if(energy <= 75)
+			batteryGUI.texture = batteryGUI75;
+		if(energy <= 50)
+			batteryGUI.texture = batteryGUI50;
+		if(energy <= 25)
+			batteryGUI.texture = batteryGUI25;
+		if(energy <= 0)
+			batteryGUI.texture = batteryGUIEmpty;	
+		if(energy >= 100)
+		{
+			energy = 100;
+			charged = true;
+		}
+		yield;
 	}
-	if(turnedOn && energy == 100)
-		TurnOnAndDrainEnergy();
 }
 //This is called from outside the script to alter the amount of energy
 static function AlterEnergy (amount : int) 
